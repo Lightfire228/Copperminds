40546a5a18eb68602d3955df9331e079d1c4e32e
Lightfire228 <lightfire228@gmail.com>
2026-04-01 22:44:07 -0400

    parse frontmatter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{env, iter, path::{Path, PathBuf}};
+use std::{env, fs, iter, path::{Path, PathBuf}};
 
+use regex::Regex;
 use walkdir::{DirEntry, IntoIter, WalkDir};
+use yaml_serde::Mapping;
 
 use crate::vault::Index;
 
@@ -13,8 +15,34 @@ fn main() {
     let index = Index::build();
 
     for f in index.md_files {
-        println!("{:?}", f.file_name());
+
+        let Some(text) = get_frontmatter_text(&f) else {
+            continue;
+        };
+
+        parse_frontmatter(&text);
+
+
     }
 
 }
 
+
+fn get_frontmatter_text(file: &DirEntry) -> Option<String> {
+
+    // TODO: https://docs.rs/regex/latest/regex/#avoid-re-compiling-regexes-especially-in-a-loop
+
+    // m     multi-line mode: ^ and $ match begin/end of line
+    // s     allow . to match \n
+    let re       = Regex::new(r"(?ms)^---\s*(.+?)^---\s*$(.*)").unwrap();
+
+    let text     = fs::read_to_string(file.path()).unwrap();
+
+    let captures = re.captures(&text)?;
+
+    Some(captures[1].to_owned())
+}
+
+fn parse_frontmatter(text: &str) -> Option<Mapping> {
+    yaml_serde::from_str::<Mapping>(text).ok()
+}
