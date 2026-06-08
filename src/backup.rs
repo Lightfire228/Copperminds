5d1899e8c1ade07e38d379430da5514e02246267
Lightfire228 <lightfire228@gmail.com>
2026-06-08 00:02:43 -0400

    fix git

--- src/backup.rs
+++ src/backup.rs
@@ -1,15 +1,21 @@
-use std::{env, ffi::OsStr, path::Path, process::Command};
+use std::{env, ffi::OsStr, path::Path, process::{Command, Output, Stdio}};
 
 pub fn backup(vault: &Path) {
     backup_named(vault, "backup")
 }
 
 pub fn backup_named(vault: &Path, commit_msg: &str) {
+
+    let branch = git(["symbolic-ref", "--short", "HEAD"], vault);
+    let branch = String::from_utf8(branch.stdout).unwrap();
+    let branch = branch.trim();
+
     git(["add",    "-A"],             vault);
     git(["commit", "-m", commit_msg], vault);
+    git(["push",   "backup", branch], vault);
 }
 
-fn git<I, S>(args: I, vault: &Path)
+fn git<I, S>(args: I, vault: &Path) -> Output
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
@@ -18,7 +24,8 @@ where
     Command::new("git")
         .args       (args)
         .current_dir(vault)
+        .stderr     (Stdio::inherit())
+        .stdout     (Stdio::piped())
         .output     ()
         .unwrap     ()
-    ;
 }
